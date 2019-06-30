71cb9eb05d2f701af0ccd8160cf65fefa329be9a
Robin Hilliard <robin@rocketboots.com>
2019-06-30 23:05:20 +1000

    Grouped airspeed, heading and altitude functions together Added yaw beta setpoint to avoid wing waggling at low speeds and support side slips Simple get_state call to support debugging

--- lib/autopilot/server.ex
+++ lib/autopilot/server.ex
@@ -13,10 +13,16 @@ defmodule Autopilot.Server do
   def start_link(instance, opts \\ []) do
     GenServer.start_link(__MODULE__, instance, [name: name(instance)] ++ opts)
   end
+  
+  
+  def get_state(pid) do
+    GenServer.call(pid, :get_state)
+  end
 
   
+  @impl true
   def init(aircraft) do
-    schedule_update
+    schedule_update()
     {
       :ok,
       State.init(aircraft: aircraft, ap_mode: :on, ap_heading_mode: :on)
@@ -27,23 +33,26 @@ defmodule Autopilot.Server do
   end
 
   
+  @impl true
   def handle_call(_msg, _from, state) do
-    {:reply, :ok, state}
+    {:reply, state, state}
   end
 
   
+  @impl true
   def handle_cast(_msg, state) do
     {:noreply, state}
   end
   
   
+  @impl true
   def handle_info(:check, state = %State{ap_mode: :off}) do
-    schedule_update
+    schedule_update()
     state
   end
   
   def handle_info(:check, state = %State{ap_mode: :on}) do
-    schedule_update
+    schedule_update()
     {
       :noreply,
       state
@@ -54,27 +63,37 @@ defmodule Autopilot.Server do
     }
   end
   
+  defp schedule_update do
+    Process.send_after(self(), :check, 20)
+  end
+  
+  
+  defp name(instance) do
+    String.to_atom("#{__MODULE__}_#{instance.addr}")
+  end
+  
+  
+  # AIRSPEED
   
   defp init_airspeed_mode(state) do
     state
   end
   
   
+  defp do_airspeed_mode(state = %State{ap_airspeed_mode: :off}) do
+    state
+  end
+  
+  
+  # HEADING
+  
   defp init_heading_mode(state) do
     state
     |> PID.add_pid(:mag_psi, :mag_psi_setpoint, :phi_setpoint,
-         p: 2.0, output_min: -30.0, output_max: 30.0)
-    |> PID.add_pid(:phi, :phi_setpoint, :aileron_trim, p: 1.0)
-  end
-  
-  
-  defp init_altitude_mode(state) do
-    state
-  end
-  
-  
-  defp do_airspeed_mode(state = %State{ap_airspeed_mode: :off}) do
-    state
+         p: 1.0, d: -0.5, output_min: -30.0, output_max: 30.0, modulo: 360.0)
+    |> PID.add_pid(:phi, :phi_setpoint, :aileron_trim,
+         p: 0.5, output_min: -1.0, output_max: 1.0)
+    |> PID.add_pid(:beta, :beta_setpoint, :rudder_trim, p: 1.0)
   end
   
   
@@ -86,22 +105,20 @@ defmodule Autopilot.Server do
     state
     |> PID.set_pid_output(:mag_psi, :mag_psi_setpoint, :phi_setpoint)
     |> PID.set_pid_output(:phi, :phi_setpoint, :aileron_trim)
-    |> State.set_output([:aileron_trim])
+    |> PID.set_pid_output(:beta, :beta_setpoint, :rudder_trim)
+    |> State.set_output([:aileron_trim, :rudder_trim])
+  end
+  
+  
+  # ALTITUDE
+  
+  defp init_altitude_mode(state) do
+    state
   end
   
   
   defp do_altitude_mode(state = %State{ap_altitude_mode: :off}) do
     state
-  end
-  
-  
-  defp schedule_update do
-    Process.send_after(self(), :check, 20)
-  end
-  
-  
-  defp name(instance) do
-    String.to_atom("#{__MODULE__}_#{instance.addr}")
   end
   
 end
